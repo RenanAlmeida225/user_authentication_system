94689b06a1a7aa2e79add55ca7285d8ec08562b2
Italo Renan <italorenan225@gmail.com>
2022-11-03 09:27:43 -0300

    created class for login of users

---
+++ src/service/signin.ts
@@ -0,0 +1,33 @@
+import { User } from '../repository/protocol/user';
+import { UserRepository } from '../repository/protocol/userRepository';
+import { MissingParamError, InvalidParamError } from '../util/error/index';
+import { genToken, VerifyPasswordHash } from '../util/helper/protocol/index';
+
+export class Singin {
+	readonly #userRepository: UserRepository;
+	readonly #gentoken: genToken;
+	readonly #verifyPassword: VerifyPasswordHash;
+	constructor(
+		userRepository: UserRepository,
+		genToken: genToken,
+		verifyPassword: VerifyPasswordHash
+	) {
+		this.#userRepository = userRepository;
+		this.#gentoken = genToken;
+		this.#verifyPassword = verifyPassword;
+	}
+
+	async loadUser({
+		email,
+		password
+	}: Omit<User, 'id' | 'userName'>): Promise<string> {
+		if (!email || !password) {
+			throw new MissingParamError();
+		}
+		const user = await this.#userRepository.load({ email, password });
+		if (!this.#verifyPassword.verified(password, user.password)) {
+			throw new InvalidParamError();
+		}
+		return this.#gentoken.gen(user.id);
+	}
+}
