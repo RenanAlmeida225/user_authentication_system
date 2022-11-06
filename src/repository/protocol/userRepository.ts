027c932e3a98ce3ae294327405e3b38eeb59d845
Italo Renan <italorenan225@gmail.com>
2022-11-06 18:20:44 -0300

    simplified params in load

--- src/repository/protocol/userRepository.ts
+++ src/repository/protocol/userRepository.ts
@@ -2,5 +2,5 @@ import { User } from './user';
 
 export interface UserRepository {
 	save(user: User): Promise<null>;
-	load({ email, password }: Omit<User, 'id' | 'userName'>): Promise<User>;
+	load(email: string): Promise<User>;
 }
