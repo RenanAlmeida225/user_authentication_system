5f955cf2bdf55c826c28739e91de085ca699ef6f
Italo Renan <italorenan225@gmail.com>
2022-11-16 09:32:27 -0300

    created config for morgan and cors

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,10 +1,15 @@
 import express, { json, urlencoded } from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
 import authRoute from './router/authRoute';
 
 const app = express();
 
 app.use(urlencoded({ extended: true }));
 app.use(json());
+app.use(cors());
+app.use(morgan('dev'));
 
 app.use('/api/v1/auth', authRoute);
 
